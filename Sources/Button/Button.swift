d5254e40ce7e46eaa0b4ed3ce1213ce7a7faf966
Lukas Lipka <lukaslipka@gmail.com>
2020-08-08 23:42:39 +0200

    Add support for setting border color.

--- Sources/Button/Button.swift
+++ Sources/Button/Button.swift
@@ -7,6 +7,10 @@ open class Button: UIButton {
         didSet { apply() }
     }
 
+    private var borderColors = [UInt: UIColor]() {
+        didSet { apply() }
+    }
+
     open override var isSelected: Bool {
         didSet { apply() }
     }
@@ -28,13 +32,27 @@ open class Button: UIButton {
         return alignmentEgdeInsets
     }
 
+    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        apply()
+    }
+
     open func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
         backgroundColors[state.rawValue] = color
+    }
+
+    open func setBorderColor(_ color: UIColor, for state: UIControl.State) {
+        borderColors[state.rawValue] = color
     }
 
     private func apply() {
         let stateBackgroundColor = backgroundColors[state.rawValue]
         let normalBackgroundColor = backgroundColors[UIControl.State.normal.rawValue]
         backgroundColor = stateBackgroundColor ?? normalBackgroundColor
+
+        let stateBorderColor = borderColors[state.rawValue]
+        let normalBorderColor = borderColors[UIControl.State.normal.rawValue]
+        layer.borderColor = stateBorderColor?.cgColor ?? normalBorderColor?.cgColor
     }
 }
